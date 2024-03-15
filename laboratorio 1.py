3b45dbe402f69a0f9d45d21b24caf6a177c27292
AngelRodas <138940596+AngelRodas@users.noreply.github.com>
2024-03-15 08:22:13 -0600

    Add files via upload
    Se crea la opcion de ingresar usuario

---
+++ laboratorio 1.py
@@ -0,0 +1,21 @@
+u="BCC-2215"
+c="225125"
+
+while True:
+    print("Ingrese su usuario:")
+    u1= input()
+    print("Ingresa la contraseña:")
+    c1 = input()
+    if c == c1 and u1==u:
+        print("Contraseña correcta y usuario correccto bienvenido")
+        break
+    else:
+        print("Contraseña incorrecta y/o usuario incorrecto. Intenta de nuevo.")
+        
+print("Seleccione una acción:")
+print("1)  Retirar" 
+       " "  "4)Estado de la cuenta"
+      "" "5)Cancelar operación"
+      )
+print("2) Depositar")
+print("3) Realizar una transferencia" )
